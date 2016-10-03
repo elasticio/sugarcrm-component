78bedaf11b1f9580914132cb6428e517f54fb11d
Roman Bolgov <bolgovr@gmail.com>
2016-10-03 20:04:56 +0300

    remove uneccessary test

--- spec/actions/query.js
+++ spec/actions/query.js
@@ -35,37 +35,6 @@ describe('sugarCRM run filter query', function () {
         });
     });
 
-    it('should emit msg, snapshot, end events on success update request', function () {
-        nock('https://test.com/')
-            .post('/rest/v10/oauth2/token')
-            .reply(200, {access_token: 1})
-            .post('/rest/v10/Tasks/filter')
-            .reply(200, require('../data/list.out.json').records[0]);
-
-        runs(function(){
-            action.process.call(
-                self,
-                {body: {module: 'Tasks', query: JSON.stringify({filter: [{}]})}},
-                cfg,
-                {test: 'c24c4069-7092-b95d-c040-523cc74a3d06'}
-            );
-        });
-
-        waitsFor(function(){
-            return self.emit.calls.length;
-        });
-
-        runs(function(){
-            var calls = self.emit.calls;
-            expect(calls.length).toEqual(3);
-            expect(calls[0].args[0]).toEqual('data');
-            expect(calls[1].args[0]).toEqual('updateSnapshot');
-            expect(calls[1].args[1].$set.test)
-                .toEqual('c24c4069-7092-b95d-c040-523cc74a3d06');
-            expect(calls[2].args[0]).toEqual('end');
-        });
-    });
-
     it('should correctly format module param', function () {
         nock('https://test.com/')
             .post('/rest/v10/oauth2/token')
