46e3849f1e6c76d2786d619a26c3be19b0f28b4d
Jacob Horbulyk <jacob.horbulyk@elastic.io>
2017-08-17 11:55:45 +0200

    Get first test running.

--- spec/triggers/getAccounts.spec.js
+++ spec/triggers/getAccounts.spec.js
@@ -10,11 +10,11 @@ describe('sugarCRM query accounts', function () {
     beforeEach(function() {
         self = jasmine.createSpyObj('self', ['emit']);
     });
-    it('should emit msg, snapshot, end events on success request', function () {
+    iit('should emit msg, snapshot events on success request', function () {
         nock('https://test.com/')
             .post('/rest/v10/oauth2/token')
             .reply(200, {access_token: 1})
-            .get('/rest/v10/Accounts')
+            .post('/rest/v10/Accounts')
             .reply(200, require('../data/list.out.json'));
 
         runs(function(){
@@ -22,16 +22,15 @@ describe('sugarCRM query accounts', function () {
         });
 
         waitsFor(function(){
-            return self.emit.calls.length;
+            return self.emit.calls.length > 0;
         });
 
         runs(function(){
             var calls = self.emit.calls;
-            expect(calls.length).toEqual(23);
-            expect(calls[21].args[0]).toEqual('snapshot');
-            expect(calls[21].args[1]['c24c4069-7092-b95d-c040-523cc74a3d06'])
-                .toEqual('e63371ee008f60dd4120b752edd17758');
-            expect(calls[22].args[0]).toEqual('end');
+            expect(calls.length).toEqual(2);
+            expect(calls[0].args[0]).toEqual('data');
+            expect(calls[0].args[1].body.data.length).toEqual(20);
+            expect(calls[1].args[0]).toEqual('snapshot');
         });
     });
 
