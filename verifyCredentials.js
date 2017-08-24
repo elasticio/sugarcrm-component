da1083ee182f82c8a910766768374e0ee050b553
Jacob Horbulyk <jacob.horbulyk@elastic.io>
2017-08-24 17:13:30 +0200

    Add debug log statement.

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -4,6 +4,7 @@ module.exports = verify;
 
 async function verify(credentials) {
     try {
+        console.log('This: ' + JSON.stringify(this));
         const instance = new sugarCRM(credentials, this);
         const pingResponse = await instance.makeRequest('ping', 'GET');
         return pingResponse === 'pong';
