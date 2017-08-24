7b69272a7011c0ebb1cd4f7d16797b2962ff8129
Jacob Horbulyk <jacob.horbulyk@elastic.io>
2017-08-24 17:06:15 +0200

    Catch exception.

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -3,8 +3,13 @@ var sugarCRM = require('./lib/sugarcrm');
 module.exports = verify;
 
 async function verify(credentials) {
-    const instance = new sugarCRM(credentials, this);
-    const pingResponse = await instance.makeRequest('ping', 'GET');
-    return pingResponse === 'pong';
+    try {
+        const instance = new sugarCRM(credentials, this);
+        const pingResponse = await instance.makeRequest('ping', 'GET');
+        return pingResponse === 'pong';
+    } catch (e) {
+        console.log('Exception: ' + e.toString());
+        return false;
+    }
 }
 
