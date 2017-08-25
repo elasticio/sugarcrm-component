d90c9129ea359f5b139b918c243ae4454989ba33
Jacob Horbulyk <jacob.horbulyk@elastic.io>
2017-08-25 14:17:37 +0200

    Add messages.

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -6,7 +6,12 @@ async function verify(credentials) {
     try {
         const instance = new sugarCRM(credentials, this);
         const pingResponse = await instance.makeRequest('ping', 'GET');
-        return pingResponse === 'pong';
+        if (pingResponse === 'pong') {
+            console.log('Successfully verified credentials.');
+            return true;
+        }
+        console.log(`Error in validating credentials.  Expected pong.  Instead got: ${JSON.stringify(pingResponse)}`);
+        return false;
     } catch (e) {
         // Workaround for https://github.com/elasticio/sailor-nodejs/issues/58
         console.log(`Exception: ${e.toString()} \n ${e.stack}`);
