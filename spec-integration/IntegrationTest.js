afe0d25326ded3de0cbc204d3d34fc4d3b864ccb
Jacob Horbulyk <jacob@elastic.io>
2018-05-04 11:33:57 +0200

    Update integration tests to handle SugarCRM 8.

--- spec-integration/IntegrationTest.js
+++ spec-integration/IntegrationTest.js
@@ -14,196 +14,208 @@ const fs = require('fs');
 
 describe('Integration Test', function GetEntryTest() {
     let username;
-    let password;
+    let password7;
+    let password8;
     let appId;
     let appSecret;
-    let sugarDoman;
+    let sugarDomain7;
+    let sugarDomain8;
     let platform;
     let cfg;
 
     this.timeout(10000);
 
-    before(function Setup() {
-        if (fs.existsSync('.env')) {
-            require('dotenv').config();
-        }
+    if (fs.existsSync('.env')) {
+        require('dotenv').config();
+    }
 
-        username = process.env.SUGAR_USERNAME;
-        password = process.env.PASSWORD;
-        appId = process.env.OAUTH_APPLICATION_ID;
-        appSecret = process.env.OAUTH_APPLICATION_SECRET;
-        sugarDoman = process.env.SUGAR_URL;
-        platform = process.env.PLATFORM;
-    });
+    username = process.env.SUGAR_USERNAME;
+    password7 = process.env.PASSWORD_7;
+    password8 = process.env.PASSWORD_8;
+    appId = process.env.OAUTH_APPLICATION_ID;
+    appSecret = process.env.OAUTH_APPLICATION_SECRET;
+    sugarDomain7 = process.env.SUGAR_7_URL;
+    sugarDomain8 = process.env.SUGAR_8_URL;
+    platform = process.env.PLATFORM;
 
-    beforeEach(function BeforeEach() {
-        cfg = {
+    const cfgs = [
+        {
             clientID: appId,
             clientSecret: appSecret,
             userName: username,
-            password: password,
-            baseUrl: sugarDoman,
+            password: password7,
+            baseUrl: sugarDomain7
+        }, {
+            clientID: appId,
+            clientSecret: appSecret,
+            userName: username,
+            password: password8,
+            baseUrl: sugarDomain8,
             platform
-        };
-    });
+        }
+    ];
 
-    describe('Webhook setup tests', function SetupWebhookTests() {
-        it('Webhook Startup - Shutdown', async function StartupShutdownTest() {
-            cfg.module = 'Contacts';
-            [getObjectsWebhook, getDeletedObjectsWebhook].forEach(async (webhook) => {
-                const result = await webhook.startup.call(undefined, cfg);
-                await webhook.shutdown.call(undefined, cfg, result);
+    cfgs.forEach(cfg => {
+        describe('Webhook setup tests', function SetupWebhookTests() {
+            it('Webhook Startup - Shutdown', async function StartupShutdownTest() {
+                cfg.module = 'Contacts';
+                [getObjectsWebhook, getDeletedObjectsWebhook].forEach(async (webhook) => {
+                    const result = await webhook.startup.call(undefined, cfg);
+                    await webhook.shutdown.call(undefined, cfg, result);
+                });
             });
         });
-    });
 
-    describe('Get Entry Tests', function GetEntryTests() {
-        it('Get Entry', async function GetEntryTests() {
-            const emitter = new TestEmitter();
-            const msg = {};
-            cfg.module = 'Contacts';
-            cfg.maxNum = '1';
+        describe('Get Entry Tests', function GetEntryTests() {
+            it('Get Entry', async function GetEntryTests() {
+                const emitter = new TestEmitter();
+                const msg = {};
+                cfg.module = 'Contacts';
+                cfg.maxNum = '1';
 
-            const initialSnapshot = undefined;
+                const initialSnapshot = undefined;
 
-            await getObjectsPolling.process.call(emitter, msg, cfg, initialSnapshot);
+                await getObjectsPolling.process.call(emitter, msg, cfg, initialSnapshot);
 
-            expect(emitter.snapshot.length).to.equal(1);
-            expect(emitter.data.length).to.equal(1);
-            expect(emitter.data[0]).to.be.a('object');
+                expect(emitter.snapshot.length).to.equal(1);
+                expect(emitter.data.length).to.equal(1);
+                expect(emitter.data[0]).to.be.a('object');
 
-            cfg.oauth = emitter.keys[0].oauth;
-            delete cfg.maxNum;
+                cfg.oauth = emitter.keys[0].oauth;
+                delete cfg.maxNum;
 
-            await getObjectsPolling.process.call(emitter, msg, cfg, emitter.snapshot[0]);
+                await getObjectsPolling.process.call(emitter, msg, cfg, emitter.snapshot[0]);
 
-            expect(emitter.data.length).to.be.above(1);
-            expect(emitter.data[1]).to.be.a('object');
-            expect(emitter.data[1]).to.not.deep.equal(emitter.data[0]);
-        });
-    });
-
-    describe('Metadata tests', function MetadataTests() {
-        it('Get Readable Modules', async function GetReadableModules() {
-            const instance = new SugarCrm(cfg, this);
-            const modules = await instance.getModules(true);
-
-            expect(modules).to.include.keys({
-                Contacts: 'Contacts',
-                Audit: 'Audit'
+                expect(emitter.data.length).to.be.above(1);
+                expect(emitter.data[1]).to.be.a('object');
+                expect(emitter.data[1]).to.not.deep.equal(emitter.data[0]);
             });
-            expect(modules).to.not.have.any.keys('_hash', 'MergeRecords');
         });
 
-        it('Get Writable Modules', async function GetWritableModules() {
-            const instance = new SugarCrm(cfg, this);
-            const modules = await instance.getModules(false);
+        describe('Metadata tests', function MetadataTests() {
+            it('Get Readable Modules', async function GetReadableModules() {
+                const instance = new SugarCrm(cfg, this);
+                const modules = await instance.getModules(true);
 
-            expect(modules).to.include.keys({
-                Contacts: 'Contacts'
+                expect(modules).to.include.keys({
+                    Contacts: 'Contacts',
+                    Audit: 'Audit'
+                });
+                expect(modules).to.not.have.any.keys('_hash', 'MergeRecords');
             });
-            expect(modules).to.not.have.any.keys('_hash', 'MergeRecords', 'Audit');
+
+            it('Get Writable Modules', async function GetWritableModules() {
+                const instance = new SugarCrm(cfg, this);
+                const modules = await instance.getModules(false);
+
+                expect(modules).to.include.keys({
+                    Contacts: 'Contacts'
+                });
+                expect(modules).to.not.have.any.keys('_hash', 'MergeRecords', 'Audit');
+            });
+
+            it('Build in schema', async function BuildInSchemaTest() {
+                cfg.module = 'Contacts';
+                const schema = await upsertObject.getMetaModel(cfg);
+
+                expect(schema.in.properties.last_name.required).to.be.true;
+                expect(schema.in.properties.date_modified).to.not.exist;
+                expect(schema.in.properties.name).to.not.exist;
+                expect(schema.in.properties._hash).to.not.exist;
+                expect(schema.in.properties.salutation.enum).to.include.members(['Mr.']);
+
+                expect(schema.in.properties.id.required).to.be.false;
+            });
+
+            it('Build out schema', async function BuildOutSchemaTest() {
+                cfg.module = 'Contacts';
+                const schema = await getObjectsWebhook.getMetaModel(cfg);
+
+                expect(schema.out.properties.id.required).to.be.true;
+                expect(schema.out.properties.date_modified).to.exist;
+            });
         });
 
-        it('Build in schema', async function BuildInSchemaTest() {
-            cfg.module = 'Contacts';
-            const schema = await upsertObject.getMetaModel(cfg);
+        describe('Token Exchange tests', function VerifyTokenExchangeTests() {
+            it('Token tests', async function TokenExchangeTests() {
+                // First test password -> token exchange
+                const emitter = new TestEmitter();
+                const instance1 = new SugarCrm(cfg, emitter);
 
-            expect(schema.in.properties.last_name.required).to.be.true;
-            expect(schema.in.properties.date_modified).to.not.exist;
-            expect(schema.in.properties.name).to.not.exist;
-            expect(schema.in.properties._hash).to.not.exist;
-            expect(schema.in.properties.salutation.enum).to.include.members(['Mr.']);
+                await instance1.makeRequest('ping', 'GET');
 
-            expect(schema.in.properties.id.required).to.be.false;
+                expect(emitter.keys.length).to.equal(1);
+                const accessToken1 = emitter.keys[0].oauth.access_token;
+                expect(accessToken1).to.exist;
+            });
         });
 
-        it('Build out schema', async function BuildOutSchemaTest() {
-            cfg.module = 'Contacts';
-            const schema = await getObjectsWebhook.getMetaModel(cfg);
+        describe('Verify Credentials Tests', function VerifyCredentialsTests() {
+            it('Correct Password', async function CorrectPasswordTest() {
+                const authResult = await verifyCredentials.call({}, cfg);
+                expect(authResult).to.be.true;
+            });
 
-            expect(schema.out.properties.id.required).to.be.true;
-            expect(schema.out.properties.date_modified).to.exist;
-        });
-    });
-
-    describe('Token Exchange tests', function VerifyTokenExchangeTests() {
-        it('Token tests', async function TokenExchangeTests() {
-            // First test password -> token exchange
-            const emitter = new TestEmitter();
-            const instance1 = new SugarCrm(cfg, emitter);
-
-            await instance1.makeRequest('ping', 'GET');
-
-            expect(emitter.keys.length).to.equal(1);
-            const accessToken1 = emitter.keys[0].oauth.access_token;
-            expect(accessToken1).to.exist;
-        });
-    });
-
-    describe('Verify Credentials Tests', function VerifyCredentialsTests() {
-        it('Correct Password', async function CorrectPasswordTest() {
-            const authResult = await verifyCredentials.call({}, cfg);
-            expect(authResult).to.be.true;
+            it('Incorrect Password', async function IncorrectPasswordTest() {
+                const wrongCfg = JSON.parse(JSON.stringify(cfg));
+                wrongCfg.password = 'WrongPassword';
+                const authResult = await verifyCredentials.call({}, wrongCfg);
+                expect(authResult).to.be.false;
+            });
         });
 
-        it('Incorrect Password', async function IncorrectPasswordTest() {
-            cfg.password = 'WrongPassword';
-            const authResult = await verifyCredentials.call({}, cfg);
-            expect(authResult).to.be.false;
-        });
-    });
+        describe('Action Tests', function ActionTests() {
+            it('Create Contact, Then Update and then Delete', async function CreateUpdateDeleteContact() {
+                const emitter = new TestEmitter();
+                const msg = {
+                    body: {
+                        first_name: 'CreateIntegration',
+                        last_name: 'TestContact',
+                        description: `Created at ${(new Date()).toISOString()} through automated integration tests`
+                    }
+                };
+                cfg.module = 'Contacts';
 
-    describe('Action Tests', function ActionTests() {
-        it('Create Contact, Then Update and then Delete', async function CreateUpdateDeleteContact() {
-            const emitter = new TestEmitter();
-            const msg = {
-                body: {
-                    first_name: 'CreateIntegration',
-                    last_name: 'TestContact',
-                    description: `Created at ${(new Date()).toISOString()} through automated integration tests`
-                }
-            };
-            cfg.module = 'Contacts';
+                await upsertObject.process.call(emitter, msg, cfg);
 
-            await upsertObject.process.call(emitter, msg, cfg);
+                const newEntry = emitter.data[0].body;
+                const originalId = newEntry.id;
+                expect(originalId).to.exist;
 
-            const newEntry = emitter.data[0].body;
-            const originalId = newEntry.id;
-            expect(originalId).to.exist;
+                newEntry.description = `${newEntry.description}\nUpdated at ${(new Date()).toISOString()}`;
 
-            newEntry.description = `${newEntry.description}\nUpdated at ${(new Date()).toISOString()}`;
+                await upsertObject.process.call(emitter, {
+                    body: newEntry
+                }, cfg);
+                const updatedEntry = emitter.data[1].body;
 
-            await upsertObject.process.call(emitter, {
-                body: newEntry
-            }, cfg);
-            const updatedEntry = emitter.data[1].body;
+                expect(updatedEntry.id).to.exist;
+                expect(updatedEntry.id).to.be.equal(originalId);
 
-            expect(updatedEntry.id).to.exist;
-            expect(updatedEntry.id).to.be.equal(originalId);
+                await deleteObject.process.call(emitter, {
+                    body: updatedEntry
+                }, cfg);
 
-            await deleteObject.process.call(emitter, {
-                body: updatedEntry
-            }, cfg);
+                expect(emitter.data.length).to.equal(3);
+                expect(emitter.data[2].body.id).to.be.equal(originalId);
+            });
 
-            expect(emitter.data.length).to.equal(3);
-            expect(emitter.data[2].body.id).to.be.equal(originalId);
-        });
+            it('Lookup test', async function LookupTest() {
+                const idToLookup = cfg.platform ? process.env.CONTACT_ID_8 : process.env.CONTACT_ID_7;
+                const emitter = new TestEmitter();
+                const msg = {
+                    body: {
+                        id: idToLookup
+                    }
+                };
+                cfg.module = 'Contacts';
 
-        it('Lookup test', async function LookupTest() {
-            const idToLookup = '64cdddcc-b7fa-11e7-b68e-02e359029409';
-            const emitter = new TestEmitter();
-            const msg = {
-                body: {
-                    id: idToLookup
-                }
-            };
-            cfg.module = 'Contacts';
+                await lookupObject.process.call(emitter, msg, cfg);
 
-            await lookupObject.process.call(emitter, msg, cfg);
+                expect(emitter.data.length).to.equal(1);
+                expect(emitter.data[0].body.name).to.be.equal('Fred Jones');
+            });
 
-            expect(emitter.data.length).to.equal(1);
-            expect(emitter.data[0].body.name).to.be.equal('Fred Jones');
         });
     });
 });
