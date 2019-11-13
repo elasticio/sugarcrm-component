dd500a7dc9b234f8b6bb0621426c32ec48903ab4
Roman Dorin <rdorin@magicsoftware.com>
2019-11-13 17:32:24 +0300

    ESLint code compliance Removed tests from ESLint checking

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,20 +1,18 @@
-'use strict';
-
 const SugarCRM = require('./lib/sugarcrm');
 
 module.exports = async function Verify(credentials) {
-    try {
-        const instance = new SugarCRM(credentials, this);
-        const pingResponse = await instance.makeRequest('ping', 'GET');
-        if (pingResponse === 'pong') {
-            console.log('Successfully verified credentials.');
-            return true;
-        }
-        console.log(`Error in validating credentials.  Expected pong.  Instead got: ${JSON.stringify(pingResponse)}`);
-        return false;
-    } catch (e) {
-        // Workaround for https://github.com/elasticio/sailor-nodejs/issues/58
-        console.log(`Exception: ${e.toString()} \n ${e.stack}`);
-        return false;
+  try {
+    const instance = new SugarCRM(credentials, this);
+    const pingResponse = await instance.makeRequest('ping', 'GET');
+    if (pingResponse === 'pong') {
+      this.logger.info('Successfully verified credentials.');
+      return true;
     }
+    this.logger.info(`Error in validating credentials.  Expected pong.  Instead got: ${JSON.stringify(pingResponse)}`);
+    return false;
+  } catch (e) {
+    // Workaround for https://github.com/elasticio/sailor-nodejs/issues/58
+    this.logger.error(`Exception: ${e.toString()} \n ${e.stack}`);
+    return false;
+  }
 };
