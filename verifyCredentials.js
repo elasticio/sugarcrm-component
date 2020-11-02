47a373e99433d5ca7e39d157684c687cf46be1f0
denis.gladikov <denis.gladikov@elastic.io>
2020-11-02 12:21:25 +0200

    Upgrade to sailor 2.6.18 Annual audit of the component code to check if it exposes a sensitive data in the logs

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -8,15 +8,15 @@ module.exports = function Verify(credentials, cb) {
         this.logger.info('Successfully verified credentials.');
         return cb(null, { verified: true });
       }
-      this.logger.info(`Error in validating credentials.  Expected pong.  Instead got: ${JSON.stringify(pingResponse)}`);
+      this.logger.info('Error in validating credentials.  Expected pong');
       return cb(null, { verified: false });
     }).catch((e) => {
-      this.logger.error(`Exception: ${e.toString()} \n ${e.stack}`);
+      this.logger.error('Verification request failed with en Exception!');
       return cb(e, { verified: false });
     });
   } catch (e) {
     // Workaround for https://github.com/elasticio/sailor-nodejs/issues/58
-    this.logger.error(`Exception: ${e.toString()} \n ${e.stack}`);
+    this.logger.error('Credentials verification failed!');
     return cb(e, { verified: false });
   }
 };
