1ef581a1b1dc86c7115efe6643b9266d4bdc5e4a
EmptyInfinity <soroka14021998@gmail.com>
2022-08-03 01:12:45 +0300

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,52 @@
 module.exports = {
   env: {
-    browser: true,
-    commonjs: true,
     es6: true,
-    mocha: true
+    node: true,
+    mocha: true,
   },
   extends: [
     'airbnb-base',
   ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'linebreak-style': 0,
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['spec/**/*', 'spec-integration/**/*'] }],
+    'no-plusplus': 0,
+    'no-unused-vars': 0,
+    'no-await-in-loop': 0,
+    'prefer-default-export': 0,
+    'import/prefer-default-export': 0,
+    'class-methods-use-this': 1,
+    'max-len': ['error', { code: 180 }],
+    'no-param-reassign': 1,
+    'no-return-assign': 1,
+    'no-use-before-define': 0,
+    'comma-dangle': 0,
+    'object-curly-newline': 0,
+    camelcase: 0,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 };
